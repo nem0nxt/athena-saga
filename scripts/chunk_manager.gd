bea6a541a1832302f2eeff24833dbb515f04fc0b
Stelios Vouzonikos <stelios@vouzonikos.com>
2026-02-09 14:51:23 +0200

    Fix: unused delta parameter -> _delta

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -48,7 +48,7 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	instance = null
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not player:
 		find_player()
 		return
